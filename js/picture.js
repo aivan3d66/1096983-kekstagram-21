7afbad2eeb38600c69c7f2b6a27baf10c19e2d92
Ivan <adamovskij00@mail.ru>
2020-10-28 12:30:41 +0300

    Добавляет изменения: перламутровые пуговицы (часть 2)

--- js/picture.js
+++ js/picture.js
@@ -3,24 +3,6 @@
 (function () {
   const bigPictureElement = document.querySelector(`.big-picture`);
   const pictureCloseButton = document.querySelector(`.big-picture__cancel`);
-  const commentTemplateNode = bigPictureElement.querySelector(`.social__comment`).cloneNode(true);
-  const fullSizeCommentsList = bigPictureElement.querySelector(`.social__comments`);
-
-  function getHtmlCommentsFragment(comments) {
-    const fragment = document.createDocumentFragment();
-
-    comments.forEach((comment) => {
-      const commentElement = commentTemplateNode.cloneNode(true);
-
-      commentElement.querySelector(`.social__picture`).src = comment.avatar;
-      commentElement.querySelector(`.social__picture`).alt = comment.name;
-      commentElement.querySelector(`.social__text`).textContent = comment.message;
-
-      fragment.appendChild(commentElement);
-    });
-
-    return fragment;
-  }
 
   function galleryEscKeydownHandler(evt) {
     if (window.utils.isEscButton(evt.key)) {
@@ -29,15 +11,12 @@
   }
 
   function renderPicture(picture) {
-    const commentsHtmlFragment = getHtmlCommentsFragment(picture.comments);
-
     bigPictureElement.querySelector(`.big-picture__img`).querySelector(`img`).src = picture.url;
     bigPictureElement.querySelector(`.likes-count`).textContent = picture.likes;
     bigPictureElement.querySelector(`.comments-count`).textContent = picture.comments.length;
     bigPictureElement.querySelector(`.social__caption`).textContent = picture.description;
 
-    fullSizeCommentsList.textContent = ``;
-    fullSizeCommentsList.appendChild(commentsHtmlFragment);
+    window.comments.set(picture.comments);
   }
 
   function openPicture() {
